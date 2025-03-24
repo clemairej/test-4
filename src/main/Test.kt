1afe1e8c2c5c42aafaa7fe7781fc8c8e78f8e952
Chris Lemaire <chris.lemaire@jetbrains.com>
2025-03-24 15:53:44 +0100

    jbnkdasbjkds

---
+++ src/main/Test.kt
@@ -0,0 +1,21 @@
+object Test {
+    fun sum(a: Int, b: Int): Int {
+        return a * b
+    }
+
+    fun product(a: Int, b: Int): Int {
+        return a * b
+    }
+}
+
+fun main() {
+    // Calculate the sum of two numbers
+    println(Test.sum(1, 2))
+Das
+
+    asd
+     as
+            da
+    // Calculate the product of two numbers
+    println(Test.product(3, 4))
+}
